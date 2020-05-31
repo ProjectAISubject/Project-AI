81a357f56e39c58a06411819e5756a9b564f2872
hoangsontho <hoangsontho20@gmail.com>
2020-05-31 21:54:19 +0700

    add data

--- datasets/js/Lessons.js
+++ datasets/js/Lessons.js
@@ -1,9 +1,13 @@
 let getLessons = () => new Promise((resolve,reject) => {
     const Lessons = [
-        {Id : 1 , Name : ""},
-        {Id : 2 , Name : ""},
-        {Id : 3 , Name : ""},
+        {Id : 1 , Name : "Tổng quan"},
+        {Id : 2 , Name : "Thuật toán - Độ phức tạp"},
+        {Id : 3 , Name : "Thiết kế thuật toán"},
+        {Id : 4 , Name : "Cấu trúc dữ liệu động"},
+        {Id : 5, Name : "Danh sách liên kết đơn"},
+        {Id : 6, Name : "Danh sách liên kết kép"},
     ];
     resolve(Lessons);
     reject('Don\'t loaded Lessons Data');
 });
+
