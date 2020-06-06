e0094f1997d924fd9904a281e356c86b027896a6
kyotake35 <kyotake35@gmail.com>
2020-06-06 11:39:53 +0700

    lessons 7

--- datasets/js/Lessons.js
+++ datasets/js/Lessons.js
@@ -1,8 +1,11 @@
 let getLessons = () => new Promise((resolve,reject) => {
     const Lessons = [
-        {Id : 1 , Name : ""},
-        {Id : 2 , Name : ""},
-        {Id : 3 , Name : ""},
+        {Id : 7 , Name : "Tìm Kiếm "},
+        {Id : 8 , Name : "Sắp Xếp"},
+        {Id : 9 , Name : "Cây Nhị Phân Tìm Kiếm"},
+        {Id : 10 , Name : "Hasing"},
+        {Id : 11 , Name : "Đường Đi Ngắn Nhất"},
+        {Id : 12 , Name : "Tree"},
     ];
     resolve(Lessons);
     reject('Don\'t loaded Lessons Data');
